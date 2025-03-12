19069a4e8609bb6fcdc7df0624ed0163b1014817
1laggerman <121728629+1laggerman@users.noreply.github.com>
2025-03-12 23:48:38 +0200

    fix base run

--- Scripts/train.py
+++ Scripts/train.py
@@ -6,46 +6,50 @@ from pathlib import Path
 from utils.configs import add_run_config, create_run_dir
 from models.trainers.MyBaseTrainer import MyBaseTrainer
 
-gpu = -1
-config = Config(Path('RT_flood/configs/base_config.yaml'))
+if __name__ == '__main__':
 
-# check if a GPU has been specified as command line argument. If yes, overwrite config
-if gpu is not None and gpu >= 0:
-    config.device = f"cuda:{gpu}"
-if gpu is not None and gpu < 0:
-    config.device = "cpu"
+    # start_run(config_file=Path('RT_flood/configs/base_config.yaml'), gpu=-1)
 
-# # start training
-if config.head.lower() in ['regression', 'gmm', 'umal', 'cmal', '']:
-    trainer = MyBaseTrainer(cfg=config)
-else:
-    raise ValueError(f"Unknown head {config.head}.")
+    gpu = 0
+    config = Config(Path('RT_flood/configs/base_config.yaml'))
 
-trainer.initialize_training()
+    # check if a GPU has been specified as command line argument. If yes, overwrite config
+    if gpu is not None and gpu >= 0:
+        config.device = f"cuda:{gpu}"
+    if gpu is not None and gpu < 0:
+        config.device = "cpu"
 
-# get epoch 0 train loss
-trainer._epoch_0()
-avg_losses = trainer.experiment_logger.summarise()
-loss_str = ", ".join(f"{k}: {v:.5f}" for k, v in avg_losses.items())
-LOGGER.info(f"Epoch {0} average loss: {loss_str}")
+    # # start training
+    if config.head.lower() in ['regression', 'gmm', 'umal', 'cmal', '']:
+        trainer = MyBaseTrainer(cfg=config)
+    else:
+        raise ValueError(f"Unknown head {config.head}.")
 
-# get epoch 0 validation loss
-if (trainer.validator is not None):
-    trainer.validator.evaluate(epoch=0,
-                            save_results=trainer.cfg.save_validation_results,
-                            save_all_output=trainer.cfg.save_all_output,
-                            metrics=trainer.cfg.metrics,
-                            model=trainer.model,
-                            experiment_logger=trainer.experiment_logger.valid())
+    trainer.initialize_training()
 
-    valid_metrics = trainer.experiment_logger.summarise()
-    print_msg = f"Epoch 0 average validation loss: {valid_metrics['avg_total_loss']:.5f}"
-    # print(print_msg)
-    if trainer.cfg.metrics:
-        print_msg += f" -- Median validation metrics: "
-        print_msg += ", ".join(f"{k}: {v:.5f}" for k, v in valid_metrics.items() if k != 'avg_total_loss')
-        LOGGER.info(print_msg)
+    # get epoch 0 train loss
+    trainer._epoch_0()
+    avg_losses = trainer.experiment_logger.summarise()
+    loss_str = ", ".join(f"{k}: {v:.5f}" for k, v in avg_losses.items())
+    LOGGER.info(f"Epoch {0} average loss: {loss_str}")
 
-trainer.train_and_validate()
+    # get epoch 0 validation loss
+    if (trainer.validator is not None):
+        trainer.validator.evaluate(epoch=0,
+                                save_results=trainer.cfg.save_validation_results,
+                                save_all_output=trainer.cfg.save_all_output,
+                                metrics=trainer.cfg.metrics,
+                                model=trainer.model,
+                                experiment_logger=trainer.experiment_logger.valid())
+
+        valid_metrics = trainer.experiment_logger.summarise()
+        print_msg = f"Epoch 0 average validation loss: {valid_metrics['avg_total_loss']:.5f}"
+        # print(print_msg)
+        if trainer.cfg.metrics:
+            print_msg += f" -- Median validation metrics: "
+            print_msg += ", ".join(f"{k}: {v:.5f}" for k, v in valid_metrics.items() if k != 'avg_total_loss')
+            LOGGER.info(print_msg)
+
+    trainer.train_and_validate()
 
 
